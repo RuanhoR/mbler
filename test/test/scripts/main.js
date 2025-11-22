9ea01f272d8c78ae72155e82ac02c3473dce1f46
n304Sc-haoran <3915264929@qq.com>
2025-11-23 00:46:08 +0800

    更新测试

--- test/test/scripts/main.js
+++ test/test/scripts/main.js
@@ -1,11 +1,18 @@
 import {
-  initSAPIGame
-} from "create-game"
-initSAPIGame({
-    debugMode: true, //debugMode开关
-    onEnd: onEnd, //执行/game end时触发
-    hub: Hub, //玩家执行/hub时触发
-    onJoin(p) {
-      console.log('player join')
-    },
-});
+  GameLib
+} from "gameLib"
+const game = new GameLib(true);
+const ui = game.createFrom("Action", {
+  layout: [{
+    type: "button",
+    param: [
+      "test"
+    ]
+  }],
+  title: "Hello"
+})
+game.event.UseItem(({
+  player
+}) => {
+  ui.show(player)
+})
