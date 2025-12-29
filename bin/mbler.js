28106c51bdced70d4022dda1f4ff1b47c0e88bc9
n304Sc-haoran <3915264929@qq.com>
2025-12-29 17:43:53 +0800

    feat(commander): 统一键盘输入获取模块

--- bin/mbler.js
+++ bin/mbler.js
@@ -4,4 +4,9 @@ try {
   new main(require('path').dirname(__dirname))
 } catch (err) {
   if (err.message.includes("Cannot find module")) console.log("Plase call 'npm install'")
+  else console.log(err)
 }
+process.on('SIGINT', () => {
+  console.log('\n🛑 你按下了 Ctrl+C，程序即将退出！'+new Error().stack);
+  // 你可以在这里输出一些状态，但依然无法直接获得 “当前执行到哪一行”
+});
