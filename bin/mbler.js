fca09266e5de9621d809f4e8b11b68a9f404fd41
Ruanhor <3915264929@qq.com>
2026-01-01 18:41:15 +0800

    test(mcx)

--- bin/mbler.js
+++ bin/mbler.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+/**
+ * @license MIT
+ * @description MCBE附加包开发工具
+ * @author github-Ruanhor
+ */
 const main = require('./../lib/start');
 new main(require('path').dirname(__dirname))
