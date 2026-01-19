3e7e098d3ee2ffcfbf32a88d6f66acad35195d71
Ruanhor <3915264929@qq.com>
2026-01-19 23:13:28 +0000

    src: fix: getResConfig

--- src/start/getResConfig.ts
+++ src/start/getResConfig.ts
@@ -1,10 +1,9 @@
 import path from 'path';
-import * as utils from './../utils/index.js';
-
+import fs from "node:fs/promises"
 const cache = new Map<string, unknown>();
 
-export = async function getResConfig(dirname: string): Promise<Map<string, unknown>> {
-  cache.set('Mod', JSON.parse(await utils.readFile(path.join(dirname, './lib/modules/contents.json'), "utf-8")));
-  cache.set('innerDef', JSON.parse(await utils.readFile(path.join(dirname, './lib/modules/innerDef.json'), "utf-8")));
+export = async function getResConfig(dirname: string): Promise<Map<string, any>> {
+  cache.set('Mod', JSON.parse(await fs.readFile(path.join(dirname, './lib/modules/contents.json'), "utf-8")));
+  cache.set('innerDef', JSON.parse(await fs.readFile(path.join(dirname, './lib/modules/innerDef.json'), "utf-8")));
   return cache;
 };
