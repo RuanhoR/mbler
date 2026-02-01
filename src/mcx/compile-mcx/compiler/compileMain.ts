a2d4b29009cce676bed9e9944c1139af1f9c9c94
Ruanhor <3915264929@qq.com>
2026-02-01 18:40:35 +0800

    src

--- src/mcx/compile-mcx/compiler/compileMain.ts
+++ src/mcx/compile-mcx/compiler/compileMain.ts
@@ -3,6 +3,7 @@ import { join } from "./../../../utils"
 import * as CompileData from "./compileData"
 import * as compiler from "./"
 import { readFile } from "node:fs/promises";
+import { extname } from "node:path";
 export default class CompileMain {
   main: string = "";
   constructor(public opt: CompileOpt) {
@@ -11,6 +12,14 @@ export default class CompileMain {
   async start() {
     if (!this.main) throw new Error("[mcx load]: mcx loader must has a main file")
     const mainCode = await readFile(this.main, "utf-8");
-    compiler
+    const IR = this.compMain(mainCode);
+  }
+  private compMain(code: string): CompileData.JsCompileData {
+    const ext = extname(this.main);
+    if (ext !== ".js") {
+      throw new Error("[load project]: main file must is a javascript.");
+    }
+    const ir = compiler.compileJS(code);
+    return ir;
   }
 }
