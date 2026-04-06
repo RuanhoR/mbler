895f5f07378b430077db27b442735cd50d0b69a0
Ruanhor <3915264929@qq.com>
2026-04-06 21:54:57 +0800

    feat: add project call to mcx plugin

--- src/build/index.ts
+++ src/build/index.ts
@@ -258,6 +258,7 @@ class Build {
           mcxDef.plugin({
             moduleDir: moduleDir,
             main: main,
+            ProjectDir: this.baseBuildDir
           })
         )
       } catch (err) {
