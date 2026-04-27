8ee29d7291c0c3df2b6deb02be0cfe9ec7015dc3
Ruanhor <3915264929@qq.com>
2026-04-27 23:00:16 +0800

    chore: update version file

--- src/version.ts
+++ src/version.ts
@@ -1,10 +1,7 @@
-export default { commit: `commit 81c6f8bb0b33367bd219c7c9168f18ae83e1f2e1
+export default { commit: `commit fc1fee7f44a0ee08388276ad9288cbb5c0263414
+Merge: f36a4b5 c393c36
 Author: Ruanhor <3915264929@qq.com>
-Date:   Sun Apr 19 16:33:01 2026 +0800
+Date:   Sun Apr 26 17:05:29 2026 +0800
 
-    fix: init and add plan command
-<<<<<<< HEAD
-`, version: "0.2.3-alpha.r1" }
-=======
-`, version: "0.2.2" }
->>>>>>> c393c36c61fd36cda3bffa2cc06f0040fbd34658
+    Merge branch 'master' of github.com:RuanhoR/mbler
+`, version: "0.2.3-alpha.r2" }
