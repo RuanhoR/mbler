dd25239ca26cb15411402df19b2b6b0c1546ba63
Ruanhor <3915264929@qq.com>
2026-05-02 16:52:00 +0800

    chore: 五一快乐！\n - 删除过时workflow \n - 提升版本

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,8 @@
-export default { commit: `commit b0cfa5d611b54f5cf3f816b7497800b22faad01f
+export default { commit: `commit 4329ae59e38d365d76d4e90049a1e7bbf6348d84
 Author: Ruanhor <3915264929@qq.com>
-Date:   Fri May 1 11:23:40 2026 +0800
+Date:   Fri May 1 14:39:53 2026 +0800
 
-    fix: fix manifest generate and build bundle option effect
+    fix: fix minecraft api dependencies version generate error
     
     Co-authored-by: Copilot <copilot@github.com>
-`, version: "0.2.3-alpha.r5" }
+`, version: "0.2.4" }
