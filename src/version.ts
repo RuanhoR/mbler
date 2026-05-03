3902fa5397576a105378005ba2cafc60bf5d3b85
Ruanhor <3915264929@qq.com>
2026-05-03 11:19:17 +0800

    chore: update package

--- src/version.ts
+++ src/version.ts
@@ -1,10 +1,7 @@
-export default { commit: `commit 02e744c14499fd89e4c4050b4e21ebff05269468
+export default { commit: `commit 153677f295ca2cfc57d6457f03680bf29e523445
+Merge: 7c92569 b5911a1
 Author: Ruanhor <3915264929@qq.com>
-Date:   Sat May 2 21:52:15 2026 +0800
+Date:   Sun May 3 11:05:56 2026 +0800
 
-    docs: update readme
-<<<<<<< HEAD
-`, version: "0.2.4-rc.2" }
-=======
-`, version: "0.2.4-rc.1" }
->>>>>>> b5911a1ee7c325427efd1c1e32ed488575adf36a
+    Merge branch 'master' of github.com:RuanhoR/mbler
+`, version: "0.2.4-rc.4" }
