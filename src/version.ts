c3da52a459b77a978dde365e4baee8360453c6f8
ruanhor <3915264929@qq.com>
2026-05-13 21:38:02 +0800

    fix: fix ts path error

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
-export default { commit: `commit 8bf795f2d751aacdbb9c8c1739dce0dbdcb5baf8
+export default { commit: `commit f1c3d278fd44601634df7155395304b612b16161
 Author: Ruanhor <3915264929@qq.com>
-Date:   Tue May 5 15:28:18 2026 +0800
+Date:   Fri May 8 21:28:01 2026 +0800
 
-    feat: remove basic plugin because rolldown already covered natively and add en, ja, ko README
-`, version: "0.2.4-rc.5" }
+    feat: fix generate sapi error and add mcx template package
+`, version: "0.2.4-rc.6" }
