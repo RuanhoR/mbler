a0ebdb23d0ab621f9a8e38d3deacf6d731443d1d
ruanhor <3915264929@qq.com>
2026-05-25 22:49:28 +0800

    chore: update version and sort files

--- src/version.ts
+++
@@ -1,6 +0,0 @@
-export default { commit: `commit 8185081889a5b9abaf17cba8132cecb881652d3a
-Author: ruanhor <3915264929@qq.com>
-Date:   Sat May 23 16:06:12 2026 +0800
-
-    style: update readme and format code
-`, version: "0.2.7" }
